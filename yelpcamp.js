95dce15de58ed33dd8c2d6e8bddffae9e63c4c85
CalebH <chronos318@yahoo.com>
2021-04-11 12:31:42 -0400

    Auth Module Completed

--- yelpcamp.js
+++ yelpcamp.js
@@ -3,6 +3,7 @@ const Mongo = require('./lib/mongo');
 const mongo = new Mongo;
 const routes = require('./lib/routes');
 const seedDB = require('./lib/seed');
+const {hashPWD, login} = require('./lib/bcrypt');
 
 process.env.PORT = 3000;
 process.env.IP = 'localhost';
@@ -12,3 +13,5 @@ svr.init();
 routes(svr.app);
 svr.listen();
 // seedDB();
+// hashPWD('yelpcamp');
+// login('yelpcamp', '$2b$12$eKI7uyS5zZ3zpfFsXm.5ceRNVbIn.cA/ZXjZy8ul1Kxe99R1vuk0a');
